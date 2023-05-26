bb634b30bce60007a52f86622d318767e64e764d
Bian Lee <bianlee2004@gmail.com>
2023-05-26 10:51:30 -0700

    please work

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <>
       <section className="bg-gradient-to-bl from-violet-300 via-pink-500 to-neutral-100">
-        <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+        <div className="mx-auto lg:max-w-5xl sm:max-w-6xl md:max-w-6xl max-w-5xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
           {/* Build & Grow Your Listener Base */}
           <h2
             className="text-center text-5xl font-bold text-white tracking-tight sm:text-6xl"
@@ -15,7 +15,7 @@ export default function Header() {
           >
             <div className="inline-block mb-2">
               <div
-                class="text-white inline-block text-6xl lg:text-7xl sm:text-6xl"
+                class="text-white inline-block text-6xl lg:text-7xl sm:text-5xl"
                 style={{ lineHeight: "3.5rem" }}
               >
                 Build Empires on the Web
@@ -23,8 +23,8 @@ export default function Header() {
             </div>
             {/* <div class="text-orange-400 inline-block">AllegroStudio</div> */}
           </h2>
-          <div className="text-center mb-0 sm:mb-0 md:mb-3 lg:mb-5 sm:mt-5 lg:mt-3 mt-5">
-            <div className="text-center inline-block sm:text-xl lg:text-xl text-white tracking-tight sm:text-lg sm:mt-3 lg:mt-5">
+          <div className="mb-0 sm:mb-0 md:mb-3 lg:mb-5 sm:mt-5 lg:mt-3 mt-5">
+            <div className="text-center  inline-block sm:text-xl lg:text-xl text-white tracking-tight sm:text-lg sm:mt-3 lg:mt-5">
               At affordable budget, I&apos;ll build you a blazingly-fast,
               mobile-friendly website with: brilliant design, intuitive UI,
               simple CMS (with Google Sheets API) and effective SEO — bundled
